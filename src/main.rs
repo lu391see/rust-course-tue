2bc6549b90ed7db16d950ee5a073c0591ba7af7e
lu391see <lukas.seehuber@proton.me>
2023-04-15 18:14:30 +0200

    counter fro 0 to 9

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: i32 = 0;
+    for _i in (1..10) {
+        println!("{x}");
+        x = x + 1;
+    }
+    println!("final result: {}", x);
 }
