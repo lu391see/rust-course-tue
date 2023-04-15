4fd613d9cea7581389bb6dc3e0a26c50d59c37b4
lu391see <lukas.seehuber@proton.me>
2023-04-15 20:04:23 +0200

    first parsing example

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
+use std::str::FromStr;
+use std::env;
+
 fn main() {
-    let x: i32 = 0;
-    for i in x..=10 {
+    let start_str = match env::args().nth(1) {
+        Some(s) => s,
+        None => String::from("0"),
+    };
+    let stop_str = match env::args().nth(2) {
+        Some(s) => s,
+        None => String::from("10"),
+    };
+
+    let start: i32 = match i32::from_str(&start_str) {
+        Ok(i) => i,
+        _ => 0,
+    };
+    let stop: i32 = match i32::from_str(&stop_str) {
+        Ok(i) => i,
+        _ => 10,
+    };
+
+    for i in start..=stop {
         println!("{i}");
     };
 }
