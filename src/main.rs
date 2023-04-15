03aa9e0621753412b1db9a925616e92f3269cfcb
lu391see <lukas.seehuber@proton.me>
2023-04-15 20:29:33 +0200

    first rust tests

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn program(args: Vec<String>) {
         .iter()
         .nth(1)
         .map(|s: &String| {
-            i32::from_str(&s)
+            i32::from_str(s)
         })
         .unwrap_or(Ok(0))
         .unwrap_or_else(|err| {
@@ -16,6 +16,25 @@ fn program(args: Vec<String>) {
     for i in x..10 {
         println!("{i}");
     };
+}
+
+#[test]
+fn test_my_program(){
+    let test_vec = vec![String::from("foo"), String::from("1")];
+    program(test_vec);
+}
+
+#[test]
+fn test_my_program_42(){
+    let test_vec = vec![String::from("foo"), String::from("42")];
+    program(test_vec);
+}
+
+#[test]
+// #[should_panic] to test panic
+fn test_my_program_bar(){
+    let test_vec = vec![String::from("foo"), String::from("bar")];
+    program(test_vec);
 }
 
 fn main() {
