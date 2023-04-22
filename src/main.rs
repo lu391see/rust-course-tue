a0289a698e714e5c662b5579308e5a8cf12b029f
lu391see <lukas.seehuber@proton.me>
2023-04-22 16:59:33 +0200

    use structs

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 use std::{fmt::Error, str::FromStr};
 
-fn check_weight(weight: f64) -> Result<f64, Error> {
-    if weight < 0.0 || weight > 200.0 {
+struct Height {
+    value: f64,
+
+}
+
+struct Weight {
+    value: f64,
+
+}
+
+struct BMI {
+    value: f64,
+
+}
+
+fn check_weight(weight: Weight) -> Result<f64, Error> {
+    if weight.value < 0.0 || weight.value > 200.0 {
         eprintln!("The given weght is illegal! Needs to be within (0, 200)");
         Err(Error)
     } else {
-        Ok(weight)
+        Ok(weight.value)
     }
 }
 
-fn check_height(height: f64) -> Result<f64, Error> {
-    if height < 0.0 || height > 2.0 {
+fn check_height(height: Height) -> Result<f64, Error> {
+    if height.value < 0.0 || height.value > 2.0 {
         eprintln!(
             "The given height is illegal! Please give a height in centimeres within (0.0, 2.0)"
         );
         Err(Error)
     } else {
-        Ok(height)
+        Ok(height.value)
     }
 }
 
-fn calc_bmi(weight: f64, height: f64) -> f64 {
-    let w = check_height(height).unwrap();
-    let h = check_weight(weight).unwrap();
-    return w / (h * h);
+fn calc_bmi(w: Weight, h: Height) -> BMI {
+    // let w = check_height(height).unwrap();
+    // let h = check_weight(weight).unwrap();
+    let bmi = w.value / (h.value * h.value);
+    BMI { value: bmi, }
 }
 
 fn main() {
@@ -36,7 +52,9 @@ fn main() {
         Ok(_) => (),
         Err(error) => print!("error: {error}"),
     }
-    let weight = f64::from_str(&buf_weight.trim_end_matches('\n')).unwrap();
+    let weight = Weight {
+        value: f64::from_str(&buf_weight.trim_end_matches('\n')).unwrap(),
+    };
     println!("Your entered weight: {buf_weight} kg");
 
     println!("Please enter your height:");
@@ -45,10 +63,12 @@ fn main() {
         Ok(_) => (),
         Err(error) => print!("error: {error}"),
     }
-    let height = f64::from_str(&buf_height.trim_end_matches('\n')).unwrap();
+    let height = Height {
+        value: f64::from_str(&buf_height.trim_end_matches('\n')).unwrap(),
+    };
     println!("Your entered height: {buf_height}");
 
     // weight / height
     let bmi = calc_bmi(weight, height);
-    println!("Your BMI is {bmi}");
+    println!("Your BMI is {}", bmi.value);
 }
