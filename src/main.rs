7719f9b7cf820ddf663ddfb2c4b463412972d6a1
lu391see <lukas.seehuber@proton.me>
2023-04-22 16:38:09 +0200

    first use of cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{str::FromStr, fmt::Error};
+use std::{fmt::Error, str::FromStr};
 
 fn check_weight(weight: f64) -> Result<f64, Error> {
     if weight < 0.0 || weight > 200.0 {
@@ -11,7 +11,9 @@ fn check_weight(weight: f64) -> Result<f64, Error> {
 
 fn check_height(height: f64) -> Result<f64, Error> {
     if height < 0.0 || height > 2.0 {
-        eprintln!("The given height is illegal! Please give a height in centimeres within (0.0, 2.0)");
+        eprintln!(
+            "The given height is illegal! Please give a height in centimeres within (0.0, 2.0)"
+        );
         Err(Error)
     } else {
         Ok(height)
@@ -21,12 +23,12 @@ fn check_height(height: f64) -> Result<f64, Error> {
 fn calc_bmi(weight: f64, height: f64) -> f64 {
     let w = check_height(height).unwrap();
     let h = check_weight(weight).unwrap();
-    return w / (h*h)
+    return w / (h * h);
 }
 
 fn main() {
     let handle = std::io::stdin();
-    
+
     println!("Please enter your weight:");
     let mut buf_weight = String::new();
     // let _ =std::io::stdout().flush();
@@ -36,7 +38,6 @@ fn main() {
     }
     let weight = f64::from_str(&buf_weight.trim_end_matches('\n')).unwrap();
     println!("Your entered weight: {buf_weight} kg");
-
 
     println!("Please enter your height:");
     let mut buf_height = String::new();
@@ -50,5 +51,4 @@ fn main() {
     // weight / height
     let bmi = calc_bmi(weight, height);
     println!("Your BMI is {bmi}");
-
 }
