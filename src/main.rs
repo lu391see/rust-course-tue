17c97cc7cdd620cb67ef50f8d18d8d9bbacdd7a5
lu391see <lukas.seehuber@proton.me>
2023-04-22 16:13:12 +0200

    bmi calc example for stdin

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,35 @@
 use std::str::FromStr;
 
-fn program(args: Vec<String>) {
-    let x = args
-        .iter()
-        .nth(1)
-        .map(|s: &String| {
-            i32::from_str(s)
-        })
-        .unwrap_or(Ok(0))
-        .unwrap_or_else(|err| {
-            println!("Error while parsing: {}", err);
-            0
-        });
+fn calc_bmi(weight: f64, height: f64) -> f64 {
 
-    for i in x..10 {
-        println!("{i}");
-    };
+    return weight / (height*height)
 }
-
 
 fn main() {
-    let args = std::env::args().collect();
-    program(args)
-}
-
-
-#[cfg(test)]
-mod test {
-    use super::program;
-        
-    #[test]
-    fn test_my_program(){
-        let test_vec = vec![String::from("foo"), String::from("1")];
-        program(test_vec);
+    let handle = std::io::stdin();
+    
+    println!("Please enter your weight:");
+    let mut buf_weight = String::new();
+    // let _ =std::io::stdout().flush();
+    match handle.read_line(&mut buf_weight) {
+        Ok(_) => (),
+        Err(error) => print!("error: {error}"),
     }
+    let weight = f64::from_str(&buf_weight.trim_end_matches('\n')).unwrap();
+    println!("Your entered weight: {buf_weight} kg");
 
-    #[test]
-    fn test_my_program_42(){
-        let test_vec = vec![String::from("foo"), String::from("42")];
-        program(test_vec);
-    }
 
-    #[test]
-    // #[should_panic] to test panic
-    fn test_my_program_bar(){
-        let test_vec = vec![String::from("foo"), String::from("bar")];
-        program(test_vec);
+    println!("Please enter your height:");
+    let mut buf_height = String::new();
+    match handle.read_line(&mut buf_height) {
+        Ok(_) => (),
+        Err(error) => print!("error: {error}"),
     }
+    let height = f64::from_str(&buf_height.trim_end_matches('\n')).unwrap();
+    println!("Your entered height: {buf_height}");
+
+    // weight / height
+    let bmi = calc_bmi(weight, height);
+    println!("Your BMI is {bmi}");
+
 }
